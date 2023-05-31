0ad22b37bcf7572c7e04154c84f98846eedc3986
2080524785 <2080524785@qq.com>
2023-05-31 22:58:20 +0800

    init

---
+++ src/main/java/com/example/meal_ordering_online/MealOrderingOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.meal_ordering_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealOrderingOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealOrderingOnlineApplication.class, args);
+    }
+
+}
